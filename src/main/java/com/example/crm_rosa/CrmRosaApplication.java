6a108437a501a0d612bb15b734c62ee7b2cd3a76
Claire T <claire.tenailleau2021@campus-eni.fr>
2022-10-19 16:44:06 +0200

    First commit

---
+++ src/main/java/com/example/crm_rosa/CrmRosaApplication.java
@@ -0,0 +1,16 @@
+package com.example.crm_rosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmRosaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmRosaApplication.class, args);
+        System.out.println("Hello");
+    }
+
+
+
+}
