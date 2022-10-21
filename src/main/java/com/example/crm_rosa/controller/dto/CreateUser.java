031de46ae6c1ad063c9b5a4531c02333ab5ef2f9
Claire T <claire.tenailleau2021@campus-eni.fr>
2022-10-21 10:17:56 +0200

    implements create User dto

---
+++ src/main/java/com/example/crm_rosa/controller/dto/CreateUser.java
@@ -0,0 +1,69 @@
+package com.example.crm_rosa.controller.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class CreateUser {
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+
+    private String pictureUrl;
+
+    MultipartFile pictureFile;
+
+    public CreateUser() {
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public MultipartFile getPictureFile() {
+        return pictureFile;
+    }
+
+    public void setPictureFile(MultipartFile pictureFile) {
+        this.pictureFile = pictureFile;
+    }
+}
