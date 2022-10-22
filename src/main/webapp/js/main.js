103de7d0c877ed958b672a54212f9413f07d22c9
Claire T <claire.tenailleau2021@campus-eni.fr>
2022-10-22 16:14:53 +0200

    implements JS method to filter search

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("clearButton")
+button.addEventListener("click", (event)=>{
+        window.location = "/enterprises/all";
+})
+
+const button = document.getElementById("alphaButton")
+button.addEventListener("click", (event)=>{
+    window.location = "/enterprises/all";
+})
+
+
+
+
